53c33c1a0a19c0eccbbb224027cc1a8c4d4fc5a5
Rica <pudimdefrutas@gmail.com>
2020-12-20 20:02:33 -0300

    Cooldown on buttons + audio

--- project/Button.gd
+++ project/Button.gd
@@ -1,5 +1,4 @@
 tool
-
 extends Control
 
 export var cooldown = 0
@@ -18,16 +17,27 @@ export var cost = {
 	"incremental_cost" : 20,
 	"exponential_progression": 1.2
 }
-
 signal acted
 
-var player = null
-var on_cooldown := false
+const HOVER_SCALE = 1.1
+const SCALE_SPEED = 3
 
+var player = null
+var hovered = false
+var on_cooldown := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Button.text = text
+
+
+func _process(dt):
+	if hovered and not $Button.disabled:
+		$Button.rect_scale.x = min($Button.rect_scale.x + SCALE_SPEED*dt, HOVER_SCALE)
+		$Button.rect_scale.y = min($Button.rect_scale.y + SCALE_SPEED*dt, HOVER_SCALE)
+	else:
+		$Button.rect_scale.x = max($Button.rect_scale.x - SCALE_SPEED*dt, 1.0)
+		$Button.rect_scale.y = max($Button.rect_scale.y - SCALE_SPEED*dt, 1.0)
 
 
 func set_player(player_ref):
@@ -39,17 +49,19 @@ func get_cost_amount():
 
 
 func error():
-	pass
+	AudioManager.play_sfx("error_button")
 
 
 func start_cooldown():
 	if not on_cooldown:
 		$Button.disabled = true
 		on_cooldown = true
-		yield(get_tree().create_timer(cooldown), "timeout")
+		$Cooldown/TextureRect/AnimationPlayer.playback_speed = 1.0/cooldown
+		$Cooldown/TextureRect/AnimationPlayer.play("Cooldown")
+		yield($Cooldown/TextureRect/AnimationPlayer, "animation_finished")
+		$Cooldown/TextureRect/AnimationPlayer.play("Idle")
 		$Button.disabled = false
 		on_cooldown = false
-		#$CooldownTween.interpolate_property()
 
 
 func _on_Button_pressed():
@@ -59,6 +71,7 @@ func _on_Button_pressed():
 		else:
 			error()
 			return
+	AudioManager.play_sfx("click_button")
 	if reward_resource.active:
 		player.get(reward_resource.type, reward_resource.amount)
 	else:
@@ -67,3 +80,13 @@ func _on_Button_pressed():
 	if cooldown:
 		start_cooldown()
 		
+
+
+func _on_Button_mouse_entered():
+	if not $Button.disabled:
+		hovered = true
+		AudioManager.play_sfx("hover_button")
+
+
+func _on_Button_mouse_exited():
+	hovered = false
