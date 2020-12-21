d2fbe19e938567bb19cca7d1fe0b20148b85d6a1
Rica <pudimdefrutas@gmail.com>
2020-12-21 17:10:23 -0300

    First sequence of palletes

--- project/PaletteLayer.gd
+++ project/PaletteLayer.gd
@@ -6,23 +6,15 @@ const BG = 0
 const FG = 1
 
 export(Array, Array, Color) var palettes = [
-	[Color.white, Color.black],
 	[Color.black, Color.white],
-	[Color.red, Color.black],
-	[Color("#b8c2b9"), Color("#382b26")], #Paperback2 - lospec
-	[Color("#3e232c"), Color("#edf6d6")], #Pixel ink - lospec
-	[Color("#222323"), Color("#f0f6f0")], #1bit monitor - lospec
-	[Color("#0020a5"), Color("#ff89ff")], #Gender Binary - lospec
-	[Color("#2e3037"), Color("#ebe5ce")], #Obra Dinn IBM 8503 - lospec
-	[Color("#222a3d"), Color("#edf2e2")], #note-2C - lospec
+	[Color("#0b1000"), Color("#a4b26f")], #The Night - lospec
 	[Color("#002f40"), Color("#ffd500")], #Gato Roboto Urine - lospec
-	[Color("#280e0b"), Color("#ffecc9")], #Gato Roboto Coffee Stain - lospec
-	[Color("#10368f"), Color("#ff8e42")], #Gato Roboto Port - lospec
-	[Color("#413652"), Color("#6493ff")], #The Night - lospec
+	[Color("#3e232c"), Color("#edf6d6")], #Pixel ink - lospec
+	[Color("#29161d"), Color("#fa9495")], #Gato Roboto Chewed Gum - lospec
 ]
 export var transition_duration := .5
 
-var curr_palette := 1
+var curr_palette := 0
 var curr_bg := Color.black
 var curr_fg := Color.white
 var transition_timer : float
